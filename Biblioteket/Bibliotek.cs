3b2f9de2e4aa00b547e03fe494103ac676bfad65
Elias Nielsen Andersen <elia1040@elevcampus.dk>
2022-05-30 14:50:28 +0200

    HentAlleLaanere

--- Biblioteket/Bibliotek.cs
+++ Biblioteket/Bibliotek.cs
@@ -6,29 +6,49 @@ using System.Threading.Tasks;
 
 namespace Biblioteket
 {
-    class Bibliotek
+    public class Bibliotek
     {
         string _biblioteksNavn;
+        List<Laaner> _laanere;
 
         public Bibliotek(string biblioteksNavn)
         {
             _biblioteksNavn = biblioteksNavn;
+            _laanere = new();
         }
 
         public string HentBibliotek()
         {
-            return $"Velkommen til {_biblioteksNavn} - datoen idag er: {DateTime.Now}";
+            return $"Velkommen til {_biblioteksNavn} - datoen idag er: {DateTime.Now.ToShortDateString()}";
         }
 
-        public Laaner OpretLaaner(int laanerNummer, string navn)
+        public string OpretLaaner(int laanerNummer, string navn)
         {
             Laaner laaner = new(laanerNummer, navn);
-            return laaner;
+            _laanere.Add(laaner);
+            return $"Låner oprettet med id: {laaner.LaanerNummer} og navn: {laaner.Navn}";
         }
 
         public string HentLaaner(int laanerNummer, string navn)
         {
-            return $"Lånernummer: {laanerNummer} - Navn: {navn} er låner hos {_biblioteksNavn}";
+            for (int i = 0; i < _laanere.Count; i++)
+            {
+                if (laanerNummer == _laanere[i].LaanerNummer)
+                {
+                    return $"Lånernummer: {laanerNummer} - Navn: {navn} er låner hos {_biblioteksNavn}";
+                }
+            }
+            return "Låner eksisterer ikke";
+        }
+
+        public string HentAlleLaanere()
+        {
+            string _alleLaanere = string.Empty;
+            foreach (Laaner laaner in _laanere)
+            {
+                _alleLaanere += laaner.Navn + "\n";
+            }
+            return _alleLaanere;
         }
     }
 }
--- Biblioteket/Program.cs
+++ Biblioteket/Program.cs
@@ -1,6 +1,36 @@
 ﻿using Biblioteket;
 
+Console.WriteLine("Bibliotek");
 Bibliotek bibliotek = new Bibliotek(Console.ReadLine());
 Console.WriteLine(bibliotek.HentBibliotek());
-Laaner laaner = bibliotek.OpretLaaner(Convert.ToInt32(Console.ReadLine()), Console.ReadLine());
-Console.WriteLine(bibliotek.HentLaaner(laaner.LaanerNummer, laaner.Navn));
+bool _failed = false;
+do
+{
+    try
+    {
+        Console.WriteLine("Lånenummer:");
+        int _laanerNummer;
+        int.TryParse(Console.ReadLine(), out _laanerNummer);
+        Console.WriteLine("Navn:");
+        string _navn = Console.ReadLine();
+        Console.WriteLine(bibliotek.OpretLaaner(_laanerNummer, _navn));
+    }
+    catch (NullReferenceException)
+    {
+        Console.WriteLine("Prøv igen");
+        _failed = true;
+    }
+} while (_failed);
+try
+{
+    Console.WriteLine("Lånenummer:");
+    int _laanerNummer;
+    int.TryParse(Console.ReadLine(), out _laanerNummer);
+    Console.WriteLine("Navn:");
+    string _navn = Console.ReadLine();
+    Console.WriteLine(bibliotek.HentLaaner(_laanerNummer, "a"));
+}
+catch (NullReferenceException)
+{
+    Console.WriteLine("Låner eksistrere ikke");
+}
